548e628ebf244c14b7b37f0d44939ba96f8e8113
ilyay <81148930+ilyayi@users.noreply.github.com>
2024-08-16 23:53:02 -0700

    Add files via upload

---
+++ vmd-scripts/100-box.tcl
@@ -0,0 +1,17 @@
+#Side faces
+graphics top line {0 0 0} {100 0 0}
+graphics top line {100 0 0} {100 100 0}
+graphics top line {100 100 0} {0 100 0}
+graphics top line {0 100 0} {0 0 0}
+
+# Top face
+graphics top line {0 0 100} {100 0 100}
+graphics top line {100 0 100} {100 100 100}
+graphics top line {100 100 100} {0 100 100}
+graphics top line {0 100 100} {0 0 100}
+
+# Vertical edges
+graphics top line {0 0 0} {0 0 100}
+graphics top line {100 0 0} {100 0 100}
+graphics top line {100 100 0} {100 100 100}
+graphics top line {0 100 0} {0 100 100}
---
+++ vmd-scripts/rotate.tcl
@@ -0,0 +1,4 @@
+set sel [atomselect top "all"]
+set com [measure center $sel weight mass]
+$sel moveby {30.42239 30.42239 30.42239}
+
